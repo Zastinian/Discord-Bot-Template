4fb14ad57163ce7404a9d6d36f81852793c8486e
Zastinian <zastinian@gmail.com>
2022-08-13 18:18:55 -0500

    Added messageCreate

---
+++ src/events/messageCreate.ts
@@ -0,0 +1,30 @@
+export {};
+
+import * as Types from "../types";
+import Discord from "discord.js";
+
+const PREFIX = '!' as string;
+
+module.exports = async (
+	client: Discord.Client,
+	commands: Discord.Collection<string, Types.Command>,
+	message: Discord.Message
+) => {
+	if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+	else {
+		const command = message.content
+			.toString()
+			.slice(PREFIX.length)
+			.trim()
+			.split(" ")
+			.shift()!
+			.toLowerCase();
+
+		if (!commands.has(command)) return;
+		try {
+			commands.get(command)!.run(message, client);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+};
