0ab245c7af27a0d3a48b626d6c4225355a0f4aa2
Zastinian <zastinian@gmail.com>
2022-09-12 09:11:22 -0500

    Update bot

--- src/Bot.ts
+++ src/Bot.ts
@@ -25,23 +25,16 @@ class Bot extends Client {
       .filter((file: string) => file.endsWith(".ts"))
     for (const file of commandFiles) {
       const command: Types.Command = require(`${__dirname}\\commands\\${file}`)
-
       commands.set(command.name, command)
-
       console.log(`Loaded command '${command.name}'`)
     }
     fs.readdir(__dirname + "/events/", (err: any, files: string[]) => {
       if (err) console.log(err)
-
       files.forEach((file: string) => {
         if (!file.endsWith(".ts")) return
-
         const evt: Function = require(__dirname + "/events/" + file)
-
         let evtName = file.split(".")[0]
-
         console.log(`Loaded event '${evtName}'`)
-
         this.on(evtName, evt.bind(null, this, commands))
       })
     })
