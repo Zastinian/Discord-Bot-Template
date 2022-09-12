79edda957f339e76a852316a537d51bb35a669e9
Zastinian <zastinian@gmail.com>
2022-09-12 09:10:55 -0500

    Update messageCreate

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -14,7 +14,6 @@ module.exports = async (
   if (!message.content.startsWith(PREFIX) || message.author.bot) return
   else {
     const command = message.content.toString().slice(PREFIX.length).trim().split(" ").shift()!.toLowerCase()
-
     if (!commands.has(command)) return
     try {
       commands.get(command)!.run(message, client)
